3ee51102e6da5bd878bef13522c7679127c76bf2
pulkit <pulkit.bosco05@gmail.com>
2012-04-05 12:04:30 +0530

    Aravind, pulkit | #1014 | plugging in bootstrap.

---
+++ ananya-bbc-reports-web/src/main/java/org/motechproject/ananya/bbc/web/CertificateReportController.java
@@ -0,0 +1,9 @@
+package org.motechproject.ananya.bbc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class CertificateReportController {
+}
