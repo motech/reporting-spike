0cded22c02f94ea537909e0095b631f43fca926e
pulkit <pulkit.bosco05@gmail.com>
2012-04-06 16:55:27 +0530

    Vijay, pulkit | #1014 | fixing build by putting dummy test in test file

--- ananya-bbc-reports-service/src/test/java/org/motechproject/ananya/bbc/service/test/CertificateCourseReportServiceTest.java
+++ ananya-bbc-reports-service/src/test/java/org/motechproject/ananya/bbc/service/test/CertificateCourseReportServiceTest.java
@@ -1,4 +1,11 @@
 package org.motechproject.ananya.bbc.service.test;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class CertificateCourseReportServiceTest {
+    
+    @Test
+    @Ignore
+    public void foo() { }
 }
