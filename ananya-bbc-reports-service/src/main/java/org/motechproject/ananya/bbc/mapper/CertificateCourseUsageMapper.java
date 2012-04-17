c5f2e4ba829bd30875639a7e1019f7325b0c452b
vijayaraj <vijayaraj@yahoo.com>
2012-04-17 15:02:31 +0530

    vijay|#00|some formatting in mapper class

--- ananya-bbc-reports-service/src/main/java/org/motechproject/ananya/bbc/mapper/CertificateCourseUsageMapper.java
+++ ananya-bbc-reports-service/src/main/java/org/motechproject/ananya/bbc/mapper/CertificateCourseUsageMapper.java
@@ -6,9 +6,16 @@ import org.motechproject.ananya.bbc.domain.CertificateCourseUsage;
 import java.util.List;
 
 public interface CertificateCourseUsageMapper {
-    Integer getCount(@Param("startTimeId") int startTimeId, @Param("endTimeId") int endTimeId);
-    List<CertificateCourseUsage> getAll(@Param("limit") int limit, @Param("offset") int offset,
-                                        @Param("startTimeId") int startTimeId, @Param("endTimeId") int endTimeId);
 
-    Integer getTimeIdFor(@Param("day") int Day, @Param("month") int month, @Param("year") int year);
+    Integer getCount(@Param("startTimeId") int startTimeId,
+                     @Param("endTimeId") int endTimeId);
+
+    List<CertificateCourseUsage> getAll(@Param("limit") int limit,
+                                        @Param("offset") int offset,
+                                        @Param("startTimeId") int startTimeId,
+                                        @Param("endTimeId") int endTimeId);
+
+    Integer getTimeIdFor(@Param("day") int Day,
+                         @Param("month") int month,
+                         @Param("year") int year);
 }
