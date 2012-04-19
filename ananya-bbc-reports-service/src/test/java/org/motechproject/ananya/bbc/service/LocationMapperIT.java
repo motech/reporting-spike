f2378eaa33dc0372631da7bf82e91aecd27a3f88
pulkit <pulkit.bosco05@gmail.com>
2012-04-19 15:16:53 +0530

    Ashok, pulkit | #1122 | Changing default location C00 to Unknown

--- ananya-bbc-reports-service/src/test/java/org/motechproject/ananya/bbc/service/LocationMapperIT.java
+++ ananya-bbc-reports-service/src/test/java/org/motechproject/ananya/bbc/service/LocationMapperIT.java
@@ -1,8 +1,11 @@
 package org.motechproject.ananya.bbc.service;
 
 import org.junit.Test;
+import org.motechproject.ananya.bbc.domain.Location;
 import org.motechproject.ananya.bbc.mapper.LocationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class LocationMapperIT extends SpringIntegrationTest {
 
@@ -11,6 +14,7 @@ public class LocationMapperIT extends SpringIntegrationTest {
 
     @Test
     public void shouldLoadLocations() {
-        locationMapper.getAll();
+        List<Location> locationList = locationMapper.getAll();
+        System.out.println(locationList);
     }
 }
